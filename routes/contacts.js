607b82bc2ce044a25ec55bed7b62d23ea1460447
Rony <rnicksonaxter@gmail.com>
2023-03-02 00:04:20 -0300

    adding project

---
+++ routes/contacts.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const contactsController = require('../controllers/contacts');
+
+router.get('/', contactsController.getContacts);
+
+router.get('/:id', contactsController.getContact);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+
+module.exports = router;
