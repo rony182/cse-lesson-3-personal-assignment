e81762fc4329e076757c33a82be9cd83979a36d4
Rony <rnicksonaxter@gmail.com>
2023-03-04 00:30:29 -0300

    adding 4rth lesson

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -27,7 +27,11 @@ const createContact = async (req, res, next) => {
         favoriteColor: req.body.favoriteColor,
         birthday: req.body.birthday
       };
-      const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+      const response = await mongodb
+        .getDb()
+        .db()
+        .collection("contacts")
+        .insertOne(contact);
       if (response.acknowledged) {
         res.status(201).json(response);
       } else {
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/contacts', require('./contacts'));
+router.use('/', require('./swagger.js'));
+
 
 module.exports = router;
