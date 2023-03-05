399208dcefa834221cb17cc89fe87517fb63a527
Rony <rnicksonaxter@gmail.com>
2023-03-04 21:49:36 -0300

    adding fixes

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
 
 const port = process.env.PORT || 8080;
+const origin = process.env.ALLOW_ORIGIN || "*";
+
 const app = express();
-const allow = process.env.ALLOW_ORIGIN || "*";
+
 app
   .use(bodyParser.json())
   .use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", allow);
+    res.setHeader("Access-Control-Allow-Origin", origin);
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, z-Key"
     );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-    );
     res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+      );
     next();
   })
   .use("/", require("./routes"));
